f50d03fb234ae56dc90b2cb9b5b85e8a7c495c6a
Suubiin <suubiin0819@gmail.com>
2024-08-27 14:43:35 +0900

    setting: 환경설정

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Main from "@/Main.jsx";
-
-function App() {
-  return <Main />;
-}
-
-export default App;
--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "@/style/index.css";
-import App from "@/App.jsx";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
