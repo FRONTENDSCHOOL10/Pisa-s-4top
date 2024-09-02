3c7ffafb1166a6857265043a1cadfdfb3753608f
woolen-h <woolen_h@kakao.com>
2024-09-02 15:22:57 +0900

    feat: 마크업 하트 버튼 추가

--- src/components/TeaRecommendCard.tsx
+++ src/components/TeaRecommendCard.tsx
@@ -1,7 +1,10 @@
 function TeaRecommendCard() {
    return (
       <article>
-         <div></div>
+         <div>
+            <img src="" alt="티 상품 미리보기" />
+         </div>
+         <button type="button"></button>
          <p>
             <h3>English Breakfast</h3>
             <p>TWINING</p>
