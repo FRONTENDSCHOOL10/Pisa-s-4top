a4e084991e7a04e63697b7a8463b2ff05e9d6f60
woolen-h <woolen_h@kakao.com>
2024-09-02 14:51:57 +0900

    feat: 마크업 중간 커밋

---
+++ src/components/TeaRecommendCard.tsx
@@ -0,0 +1,13 @@
+function TeaRecommendCard() {
+   return (
+      <article>
+         <div></div>
+         <p>
+            <h3>English Breakfast</h3>
+            <p>TWINING</p>
+         </p>
+      </article>
+   );
+}
+
+export default TeaRecommendCard;
