74dde8f1b7491e326fddb4a0625f9b0bb47b89df
Suubiin <suubiin0819@gmail.com>
2024-09-02 15:49:24 +0900

    feat: 버튼 컴포넌트 구현 및 색상 코드 추가 - Buttons.tsx: 버튼 컴포넌트(sm, md, lg, lg-error, xl) 구현 - tailwind.config.js: 색상 코드 추가

---
+++ src/components/Buttons/Buttons.tsx
@@ -0,0 +1,54 @@
+export function ButtonSmall() {
+   return (
+      <button
+         className="bg-primary text-white-default h-9 w-[5.19rem] rounded text-xs font-normal"
+         type="button"
+      >
+         버튼sm
+      </button>
+   );
+}
+
+export function ButtonMedium() {
+   return (
+      <button
+         className="bg-primary text-white-default h-[1.94rem] w-60 rounded text-xs font-normal"
+         type="button"
+      >
+         버튼md
+      </button>
+   );
+}
+
+export function ButtonLarge() {
+   return (
+      <button
+         className="bg-primary text-white-default h-[1.94rem] w-[18.5rem] rounded text-xs font-normal"
+         type="button"
+      >
+         버튼lg
+      </button>
+   );
+}
+
+export function ButtonLargeError() {
+   return (
+      <button
+         className="bg-error text-white-default h-[1.94rem] w-[18.5rem] rounded text-xs font-normal"
+         type="button"
+      >
+         버튼lg-error
+      </button>
+   );
+}
+
+export function ButtonXlarge() {
+   return (
+      <button
+         className="bg-primary text-white-default h-[2.81rem] w-[18.5rem] rounded text-xs font-normal"
+         type="button"
+      >
+         버튼xl
+      </button>
+   );
+}
