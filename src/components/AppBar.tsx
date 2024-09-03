c23b62e653f0495868e7c5c7d91b348ced24e004
minnje <167839981+minnje@users.noreply.github.com>
2024-09-03 21:37:49 +0900

    modify: app bar 디테일 수정

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,10 +1,11 @@
-import { useNavigate } from 'react-router-dom';
-
 /* 사용법
    <AppBar hasBackBtn /> => 뒤로가기 버튼만 있는 상단 바
    <AppBar hasLogo /> => 로고만 있는 상단 바
    <AppBar title="얼그레이"/> => 뒤로가기 버튼과 얼그레이가 적힌 상단 바 (title에 변수 string 가능)
+                              props 없을 시 기본값 hasBackBtn
 */
+
+import { useNavigate } from 'react-router-dom';
 
 interface AppBarProps {
    hasBackBtn?: boolean;
@@ -16,19 +17,19 @@ function AppBar({ hasBackBtn, hasLogo, title }: AppBarProps) {
    const navigate = useNavigate();
 
    return (
-      <header className="flex h-[3.25rem] w-[22.5rem] items-center bg-slate-100 py-[0.88rem] text-base">
-         <nav>
+      <header>
+         <nav className="flex h-[3.25rem] w-[22.5rem] items-center bg-slate-50 py-[0.88rem] text-base">
             {hasBackBtn ? (
                <button
                   className="p-[1rem]"
                   aria-label="뒤로가기 버튼"
                   onClick={() => navigate(-1)}
                >
-                  <span className="fi fi-rs-angle-left text-xs" />
+                  <span className="fi fi-rs-angle-left text-xs text-green-700" />
                </button>
             ) : hasLogo ? (
                <button className="mx-auto p-3">
-                  <img src="/assets/logo.svg" alt="totd 로고" />
+                  <img src="/assets/totd-logo.svg" alt="totd 로고" />
                </button>
             ) : title ? (
                <>
@@ -37,11 +38,19 @@ function AppBar({ hasBackBtn, hasLogo, title }: AppBarProps) {
                      aria-label="뒤로가기 버튼"
                      onClick={() => navigate(-1)}
                   >
-                     <span className="fi fi-rs-angle-left text-xs" />
+                     <span className="fi fi-rs-angle-left text-xs text-green-700" />
                   </button>
-                  <span>{title}</span>
+                  <span className="text-green-700">{title}</span>
                </>
-            ) : null}
+            ) : (
+               <button
+                  className="p-[1rem]"
+                  aria-label="뒤로가기 버튼"
+                  onClick={() => navigate(-1)}
+               >
+                  <span className="fi fi-rs-angle-left text-xs text-green-700" />
+               </button>
+            )}
          </nav>
       </header>
    );
