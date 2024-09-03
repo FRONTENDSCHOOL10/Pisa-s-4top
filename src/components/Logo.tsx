6ac21987e1f347f3c8e2569d5503de82fcc4971a
minnje <167839981+minnje@users.noreply.github.com>
2024-09-03 15:37:39 +0900

    modify: 로고 컴포넌트 코드 변경

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,3 +1,9 @@
+/* 사용법
+   <Logo /> => 글자 없는 잎 로고
+   <Logo small/> => totd 글자 있는 작은 로고
+   <Logo large/> => totd 글자 있는 큰 로고
+*/
+
 interface LogoProps {
    small?: boolean;
    large?: boolean;
@@ -10,7 +16,9 @@ function Logo({ small, large }: LogoProps) {
             <img src="/assets/typeLogo-sm.svg" alt="totd 로고와 글자" />
          ) : large ? (
             <img src="/assets/typeLogo-lg.svg" alt="totd 로고와 글자" />
-         ) : null}
+         ) : (
+            <img src="/assets/totd-logo.svg" alt="totd 로고" />
+         )}
       </div>
    );
 }
