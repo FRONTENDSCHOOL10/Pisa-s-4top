8eb4e72d6750c8b725becc8dcdb64a072c79e045
minnje <167839981+minnje@users.noreply.github.com>
2024-09-03 18:00:01 +0900

    modify: 시맨틱 마크업

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,9 +1,10 @@
 import { useNavigate } from 'react-router-dom';
 
-// 사용법
-// <AppBar title="얼그레이"/> => 뒤로가기 버튼과 얼그레이가 적힌 상단 바 (title에 변수로 string 가능)
-// <AppBar hasBackBtn /> => 뒤로가기 버튼만 있는 상단 바
-// <AppBar hasLogo /> => 로고만 있는 상단 바
+/* 사용법
+   <AppBar hasBackBtn /> => 뒤로가기 버튼만 있는 상단 바
+   <AppBar hasLogo /> => 로고만 있는 상단 바
+   <AppBar title="얼그레이"/> => 뒤로가기 버튼과 얼그레이가 적힌 상단 바 (title에 변수 string 가능)
+*/
 
 interface AppBarProps {
    hasBackBtn?: boolean;
@@ -11,25 +12,13 @@ interface AppBarProps {
    title?: string;
 }
 
-function AppBar({ title, hasBackBtn, hasLogo }: AppBarProps) {
+function AppBar({ hasBackBtn, hasLogo, title }: AppBarProps) {
    const navigate = useNavigate();
 
    return (
-      <div className="flex h-[3.25rem] w-[22.5rem] items-center bg-slate-200 py-[0.88rem] text-base">
-         {hasBackBtn ? (
-            <button
-               className="p-[1rem]"
-               aria-label="뒤로가기 버튼"
-               onClick={() => navigate(-1)}
-            >
-               <span className="fi fi-rs-angle-left text-xs" />
-            </button>
-         ) : hasLogo ? (
-            <button className="mx-auto p-3">
-               <img src="/assets/logo.svg" alt="totd 로고" />
-            </button>
-         ) : title ? (
-            <>
+      <header className="flex h-[3.25rem] w-[22.5rem] items-center bg-slate-100 py-[0.88rem] text-base">
+         <nav>
+            {hasBackBtn ? (
                <button
                   className="p-[1rem]"
                   aria-label="뒤로가기 버튼"
@@ -37,10 +26,24 @@ function AppBar({ title, hasBackBtn, hasLogo }: AppBarProps) {
                >
                   <span className="fi fi-rs-angle-left text-xs" />
                </button>
-               <span>{title}</span>
-            </>
-         ) : null}
-      </div>
+            ) : hasLogo ? (
+               <button className="mx-auto p-3">
+                  <img src="/assets/logo.svg" alt="totd 로고" />
+               </button>
+            ) : title ? (
+               <>
+                  <button
+                     className="p-[1rem]"
+                     aria-label="뒤로가기 버튼"
+                     onClick={() => navigate(-1)}
+                  >
+                     <span className="fi fi-rs-angle-left text-xs" />
+                  </button>
+                  <span>{title}</span>
+               </>
+            ) : null}
+         </nav>
+      </header>
    );
 }
 
