f4a0344764836b5fafd817bc8f2152238e46439a
minnje <167839981+minnje@users.noreply.github.com>
2024-09-03 15:06:02 +0900

    design: 디자인 디테일 수정

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,28 +1,41 @@
 import { useNavigate } from 'react-router-dom';
 
+// 사용법
+// <AppBar title="얼그레이"/> => 뒤로가기 버튼과 얼그레이가 적힌 상단 바 (title에 변수로 string 가능)
+// <AppBar hasBackBtn /> => 뒤로가기 버튼만 있는 상단 바
+// <AppBar hasLogo /> => 로고만 있는 상단 바
+
 interface AppBarProps {
-   title?: string;
    hasBackBtn?: boolean;
    hasLogo?: boolean;
+   title?: string;
 }
 
 function AppBar({ title, hasBackBtn, hasLogo }: AppBarProps) {
    const navigate = useNavigate();
 
    return (
-      <div className="flex h-20 w-80 items-center space-x-1 bg-slate-200 px-4 py-3 text-lg">
+      <div className="flex h-[3.25rem] w-[22.5rem] items-center bg-slate-200 py-[0.88rem] text-base">
          {hasBackBtn ? (
-            <button aria-label="뒤로가기 버튼" onClick={() => navigate(-1)}>
-               <span className="fi fi-rs-angle-left"></span>
+            <button
+               className="p-[1rem]"
+               aria-label="뒤로가기 버튼"
+               onClick={() => navigate(-1)}
+            >
+               <span className="fi fi-rs-angle-left text-xs" />
             </button>
          ) : hasLogo ? (
-            <button className="">
+            <button className="mx-auto p-3">
                <img src="/assets/logo.svg" alt="totd 로고" />
             </button>
          ) : title ? (
             <>
-               <button aria-label="뒤로가기 버튼" onClick={() => navigate(-1)}>
-                  <span className="fi fi-rs-angle-left"></span>
+               <button
+                  className="p-[1rem]"
+                  aria-label="뒤로가기 버튼"
+                  onClick={() => navigate(-1)}
+               >
+                  <span className="fi fi-rs-angle-left text-xs" />
                </button>
                <span>{title}</span>
             </>
