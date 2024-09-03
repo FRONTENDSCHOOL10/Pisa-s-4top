5ad5e681f398f070f83680cfc6e6b69b0bdb8c5a
woolen-h <woolen_h@kakao.com>
2024-09-03 17:16:46 +0900

    refactor: icon 수정

--- src/components/TeaCard/TeaRecommendCard.tsx
+++ src/components/TeaCard/TeaRecommendCard.tsx
@@ -9,7 +9,7 @@ export default function TeaRecommendCard() {
             />
          </div>
          <button className="absolute bottom-14 right-3 rounded-2xl border border-stone-300 bg-gradient-to-b from-white from-60% to-stone-100 px-1.5 py-1.5 transition hover:bg-gradient-to-b hover:from-red-600 hover:from-0% hover:to-red-400">
-            <span className="fi fi-sr-heart white flex justify-center"></span>
+            <span className="fi fi-sr-heart text-shadow-sm flex justify-center text-white"></span>
          </button>
          <div className="mt-2 p-3">
             <h3 className="mb-1 text-sm font-semibold text-stone-950">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-     extend: {
-     },
-  },
-  plugins: [],
+   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+   theme: {
+      extend: {},
+   },
+   plugins: [
+      function ({ addUtilities }) {
+         const newUtilities = {
+            '.text-shadow-sm': {
+               textShadow: '0 0 2px rgba(0, 0, 0, 0.5)',
+            },
+            '.text-shadow-md': {
+               textShadow: '0 0 4px rgba(0, 0, 0, 0.5)',
+            },
+            '.text-shadow-lg': {
+               textShadow: '0 0 6px rgba(0, 0, 0, 0.5)',
+            },
+            '.text-shadow-xl': {
+               textShadow: '0 0 8px rgba(0, 0, 0, 0.5)',
+            },
+            '.text-shadow-2xl': {
+               textShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
+            },
+            '.text-shadow-none': {
+               textShadow: 'none',
+            },
+         };
+
+         addUtilities(newUtilities, ['responsive', 'hover']);
+      },
+   ],
 };
