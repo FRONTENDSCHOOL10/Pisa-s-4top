a46d8ecf5259c5a96802a11028990d4992662082
woolen-h <woolen_h@kakao.com>
2024-09-03 23:15:12 +0900

    feat: 티 레시피 카드 생성

---
+++ src/components/TeaCard/TeaRecipeCard.tsx
@@ -0,0 +1,13 @@
+export default function TeaRecipeCard() {
+   return (
+      <article className="relative w-5/6 rounded-2xl border border-stone-300 bg-white p-5 shadow-xl">
+         <h3 className="text-sm font-extrabold text-stone-950">
+            이렇게도 드실 수 있어요!
+         </h3>
+         <div className="flex">
+            <div></div>
+            <div></div>
+         </div>
+      </article>
+   );
+}
