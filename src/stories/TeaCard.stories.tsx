998cb73d509565e53e10861f81b7ee5b3022575e
woolen-h <woolen_h@kakao.com>
2024-09-03 22:36:18 +0900

    feat: 티 리뷰 카드 마크업

--- src/stories/TeaCard.stories.tsx
+++ src/stories/TeaCard.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, StoryFn } from '@storybook/react';
 import TeaRecommendCard from '@/components/TeaCard/TeaRecommendCard';
 import TeaRecommendSwiper from '@/components/TeaCard/TeaRecommendSwiper';
 import TeaDescriptionCard from '@/components/TeaCard/TeaDescriptionCard';
+import TeaReviewCard from '@/components/TeaCard/TeaReviewCard';
 
 export default {
    title: 'Components/TeaComponents',
@@ -10,3 +11,4 @@ export default {
 export const RecommendCard: StoryFn = () => <TeaRecommendCard />;
 export const RecommendSwiper: StoryFn = () => <TeaRecommendSwiper />;
 export const DescriptionCard: StoryFn = () => <TeaDescriptionCard />;
+export const ReviewCard: StoryFn = () => <TeaReviewCard />;
