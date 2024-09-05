3727fe507e35cee07ae5ccf910545516e28e8917
woolen-h <woolen_h@kakao.com>
2024-09-05 15:40:53 +0900

    feat: 스플래쉬 스와이퍼 작업

---
+++ src/components/TeaCard/SplashSwiper.tsx
@@ -0,0 +1,59 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import 'swiper/css/free-mode';
+
+export default function SplashSwiper() {
+   return (
+      <Swiper
+         spaceBetween={16}
+         slidesPerView="auto"
+         freeMode={true}
+         loop={true}
+         autoplay={{ delay: 0, disableOnInteraction: false }}
+         centeredSlides={true}
+         style={{ overflow: 'visible' }}
+         modules={[Autoplay, FreeMode]}
+         speed={5000}
+         className="w-full"
+      >
+         <SwiperSlide
+            style={{ width: '144px', height: '144px' }}
+            className="rounded-2xl bg-stone-200"
+         >
+            1
+         </SwiperSlide>
+         <SwiperSlide
+            style={{ width: '144px', height: '144px' }}
+            className="rounded-2xl bg-stone-200"
+         >
+            2
+         </SwiperSlide>
+         <SwiperSlide
+            style={{ width: '144px', height: '144px' }}
+            className="rounded-2xl bg-stone-200"
+         >
+            3
+         </SwiperSlide>
+         <SwiperSlide
+            style={{ width: '144px', height: '144px' }}
+            className="rounded-2xl bg-stone-200"
+         >
+            4
+         </SwiperSlide>
+         <SwiperSlide
+            style={{ width: '144px', height: '144px' }}
+            className="rounded-2xl bg-stone-200"
+         >
+            5
+         </SwiperSlide>
+         <SwiperSlide
+            style={{ width: '144px', height: '144px' }}
+            className="rounded-2xl bg-stone-200"
+         >
+            6
+         </SwiperSlide>
+      </Swiper>
+   );
+}
