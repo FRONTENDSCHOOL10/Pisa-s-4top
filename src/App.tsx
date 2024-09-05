9b8f2c593375fe0f06ce096f3c39310d302cfa85
woolen-h <woolen_h@kakao.com>
2024-09-05 16:42:56 +0900

    refactor: app.tsx 리팩토링

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,108 @@
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import BottomNav from './components/BottomNav';
+import AppBar from './components/AppBar';
 
-const JoinPage = lazy(() => import('./pages/Join/JoinPage'));
-const LoginPage = lazy(() => import('./pages/Join/LoginPage'));
-const MySelectionPage = lazy(() => import('./pages/Join/MySelectionPage'));
-const MyTastePage = lazy(() => import('./pages/Join/MyTastePage'));
-const MainPage = lazy(() => import('./pages/Main/MainPage'));
-const TeaListPage = lazy(() => import('./pages/Recommend/TeaListPage'));
-const SearchPage = lazy(() => import('./pages/Main/SearchPage'));
-const ReviewsPage = lazy(() => import('./pages/Reviews/ReviewsPage'));
-const ReviewsDetailPage = lazy(
-   () => import('./pages/Reviews/ReviewsDetailPage')
-);
-const TeaDetailPage = lazy(() => import('./pages/Recommend/TeaDetailPage'));
-const ReviewsWritePage = lazy(() => import('./pages/Reviews/ReviewsWritePage'));
-const ReviewsEditPage = lazy(() => import('./pages/Reviews/ReviewsEditPage'));
-const MyPage = lazy(() => import('./pages/Settings/MyPage'));
-const MyEditPage = lazy(() => import('./pages/Settings/MyEditPage'));
-const MyReviewsPage = lazy(() => import('./pages/Settings/MyReviewsPage'));
-const MyFavoritesPage = lazy(() => import('./pages/Settings/MyFavoritesPage'));
+import {
+   LoginPage,
+   JoinPage,
+   MySelectionPage,
+   MyTastePage,
+   MainPage,
+   TeaListPage,
+   SearchPage,
+   TeaDetailPage,
+   ReviewsPage,
+   ReviewsDetailPage,
+   ReviewsWritePage,
+   ReviewsEditPage,
+   MyPage,
+   MyEditPage,
+   MyReviewsPage,
+   MyFavoritesPage,
+} from './pages';
 
-function App() {
+const routes = [
+   {
+      path: '/',
+      element: <MainPage />,
+   },
+   {
+      path: '/login',
+      element: <LoginPage />,
+   },
+   {
+      path: '/join',
+      element: <JoinPage />,
+   },
+   {
+      path: '/my-selection',
+      element: <MySelectionPage />,
+   },
+   {
+      path: '/my-taste',
+      element: <MyTastePage />,
+   },
+   {
+      path: '/recommend',
+      element: <TeaListPage />,
+   },
+   {
+      path: '/search',
+      element: <SearchPage />,
+   },
+   {
+      path: '/detail/:id',
+      element: <TeaDetailPage />,
+   },
+   {
+      path: '/reviews',
+      element: <ReviewsPage />,
+      children: [
+         {
+            path: 'detail/:id',
+            element: <ReviewsDetailPage />,
+         },
+         {
+            path: 'write',
+            element: <ReviewsWritePage />,
+         },
+         {
+            path: 'edit/:id',
+            element: <ReviewsEditPage />,
+         },
+      ],
+   },
+   {
+      path: '/my-page',
+      element: <MyPage />,
+      children: [
+         {
+            path: 'edit',
+            element: <MyEditPage />,
+         },
+         {
+            path: 'reviews',
+            element: <MyReviewsPage />,
+         },
+         {
+            path: 'favorites',
+            element: <MyFavoritesPage />,
+         },
+      ],
+   },
+];
+
+const router = createBrowserRouter(routes);
+
+const App = () => {
    return (
-      <Router>
-         <div>
-            <Suspense fallback={<div>Loading...</div>}>
-               <Routes>
-                  <Route path="/login" element={<LoginPage />} />
-                  <Route path="/join" element={<JoinPage />} />
-                  <Route path="/my-selection" element={<MySelectionPage />} />
-                  <Route path="/my-taste" element={<MyTastePage />} />
-                  <Route path="/" element={<MainPage />} />
-                  <Route path="/recommend" element={<TeaListPage />} />
-                  <Route path="/search" element={<SearchPage />} />
-                  <Route path="/detail/:id" element={<TeaDetailPage />} />
-                  <Route path="/reviews" element={<ReviewsPage />} />
-                  <Route
-                     path="/reviews/detail/:id"
-                     element={<ReviewsDetailPage />}
-                  />
-                  <Route path="/reviews/write" element={<ReviewsWritePage />} />
-                  <Route
-                     path="/reviews/edit/:id"
-                     element={<ReviewsEditPage />}
-                  />
-                  <Route path="/my-page" element={<MyPage />} />
-                  <Route path="/my-page/edit" element={<MyEditPage />} />
-                  <Route path="/my-page/reviews" element={<MyReviewsPage />} />
-                  <Route
-                     path="/my-page/favorites"
-                     element={<MyFavoritesPage />}
-                  />
-               </Routes>
-            </Suspense>
-            <BottomNav />
-         </div>
-      </Router>
+      <>
+         <AppBar />
+         <RouterProvider router={router} />
+         <BottomNav />
+      </>
    );
-}
+};
 
 export default App;
---
+++ src/pages/Reviews/index.ts
@@ -0,0 +1,4 @@
+export { default as ReviewsDetailPage } from './ReviewsDetailPage';
+export { default as ReviewsEditPage } from './ReviewsEditPage';
+export { default as ReviewsPage } from './ReviewsPage';
+export { default as ReviewsWritePage } from './ReviewsWritePage';
