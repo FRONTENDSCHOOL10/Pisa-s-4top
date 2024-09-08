ea13d2c05e2d548febc82c66b9f5f0d08ad2aa91
woolen-h <woolen_h@kakao.com>
2024-09-08 20:31:08 +0900

    feat: 리뷰 디테일 페이지 생성

--- src/pages/Reviews/ReviewsDetailPage.tsx
+++ src/pages/Reviews/ReviewsDetailPage.tsx
@@ -1,5 +1,41 @@
+import { ButtonFullWidth } from '@/components/Buttons/Buttons';
+import { ButtonError } from '@/components/Buttons/Buttons';
+import { StarRatingAverage } from '@/components/Review/StarRate';
+import { LabelGroup } from '@/components/Labels/LabelGroup';
+
 function ReviewsDetailPage() {
-   return <p>리뷰 상세 페이지</p>;
+   const labels = [
+      { label: 'Bitter' },
+      { label: 'Peanuts' },
+      { label: 'Peanuts' },
+      { label: 'Peanuts' },
+      { label: 'Peanuts' },
+      { label: 'Peanuts' },
+   ];
+
+   return (
+      <main className="flex flex-col items-center px-6">
+         <div className="h-8 w-full rounded-full bg-stone-300">
+            <img className="object-cover" src="" alt="" />
+         </div>
+         <p className="my-4">nickname</p>
+         <StarRatingAverage score={3} />
+         <article className="mt-4">
+            <h3>맛</h3>
+            <LabelGroup labels={labels} size="large" />
+         </article>
+         <article>
+            <h3>수색</h3>
+         </article>
+         <div>reviewTitle reviewContent</div>
+         <div className="mb-2 w-full">
+            <ButtonFullWidth text="리뷰 수정하기" />
+         </div>
+         <div className="w-full">
+            <ButtonError text="삭제" />
+         </div>
+      </main>
+   );
 }
 
 export default ReviewsDetailPage;
