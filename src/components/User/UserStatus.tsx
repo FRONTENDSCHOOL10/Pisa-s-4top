8a243f50267accf1ee962356d0f98d5ce50322b4
Suubiin <suubiin0819@gmail.com>
2024-09-09 15:03:58 +0900

    refactor: UserStatus 컴포넌트 피드백 반영 수정 - aria-label 삭제 - 클래스 변수로 관리

--- src/components/User/UserStatus.tsx
+++ src/components/User/UserStatus.tsx
@@ -1,25 +1,26 @@
+// 사용법
+// <UserStatus title="리뷰 개수" count={25} />
+
 export interface UserStatusProps {
    title: string;
    count: number;
-   ariaLabel?: string;
+   [props: string]: any;
 }
 
+const containerClass: string =
+   'flex w-full flex-col gap-[.625rem] rounded-2xl bg-stone-200 px-4 py-4';
+const titleClass: string = 'text-sm font-normal text-stone-600';
+const countClass: string = 'text-[2rem] font-bold leading-9 text-lime-700';
+
 export function UserStatus({
-   title,
-   count,
-   ariaLabel,
+   title = '찜 개수',
+   count = 0,
    ...restProps
 }: UserStatusProps) {
    return (
-      <div
-         className="flex w-full flex-col gap-[.625rem] rounded-2xl bg-stone-200 px-4 py-4"
-         aria-label={ariaLabel}
-         {...restProps}
-      >
-         <p className="text-sm font-normal text-stone-600">{title}</p>
-         <span className="text-[2rem] font-bold leading-9 text-lime-700">
-            {count}
-         </span>
+      <div className={containerClass} {...restProps}>
+         <p className={titleClass}>{title}</p>
+         <span className={countClass}>{count}</span>
       </div>
    );
 }
