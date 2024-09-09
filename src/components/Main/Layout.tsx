27ff1183015dd605e10584b3ee4e92cce57821d9
minnje <167839981+minnje@users.noreply.github.com>
2024-09-09 15:27:03 +0900

    modify: 상/하단바 없는 페이지 조건 처리

--- src/components/Main/Layout.tsx
+++ src/components/Main/Layout.tsx
@@ -1,15 +1,27 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import AppBar from './AppBar';
 import BottomNav from './BottomNav';
 
 export default function Layout() {
+   const location = useLocation();
+   const { pathname } = location;
+   const hideAppBar = ['/login', '/my-selection', '/my-taste'].includes(
+      pathname
+   );
+   const hideBottonNav = [
+      '/login',
+      '/join',
+      '/my-selection',
+      '/my-taste',
+   ].includes(pathname);
+
    return (
       <>
-         <AppBar />
+         {!hideAppBar && <AppBar />}
          <main className="content py-16">
             <Outlet />
          </main>
-         <BottomNav />
+         {!hideBottonNav && <BottomNav />}
       </>
    );
 }
