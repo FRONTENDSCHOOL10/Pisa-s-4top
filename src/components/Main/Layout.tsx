42ea9ab8c77b8571cf1da975cdfc3c6d5de4b120
minnje <167839981+minnje@users.noreply.github.com>
2024-09-09 16:51:21 +0900

    modify: 페이지 마다 다른 상단바 쓰도록 레이아웃 설정

--- src/components/Main/Layout.tsx
+++ src/components/Main/Layout.tsx
@@ -1,8 +1,17 @@
 import { Outlet, useLocation } from 'react-router-dom';
-import AppBar from './AppBar';
+import AppBar, { AppBarProps } from './AppBar';
 import BottomNav from './BottomNav';
 
-export default function Layout() {
+interface LayoutProps extends AppBarProps {}
+
+/* 상단 바 다른 거 쓰는 법 (기본값 hasBackBtn)
+   각 페이지 파일에서
+   <Layout hasBackBtn/> => 뒤로가기 버튼만 있는 상단 바
+   <Layout hasLogo /> => 로고만 있는 상단 바
+   <Layout title="얼그레이"/> => 뒤로가기 버튼과 얼그레이가 적힌 상단 바 (title에 변수 string 가능)
+*/
+
+export default function Layout({ hasBackBtn, hasLogo, title }: LayoutProps) {
    const location = useLocation();
    const { pathname } = location;
    const hideAppBar = ['/login', '/my-selection', '/my-taste'].includes(
@@ -17,7 +26,9 @@ export default function Layout() {
 
    return (
       <>
-         {!hideAppBar && <AppBar />}
+         {!hideAppBar && (
+            <AppBar hasBackBtn={hasBackBtn} hasLogo={hasLogo} title={title} />
+         )}
          <main className="content py-16">
             <Outlet />
          </main>
