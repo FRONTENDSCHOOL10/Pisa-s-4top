ffe9a164303364a8cd6894661f282418a065a3fc
Suubiin <suubiin0819@gmail.com>
2024-09-10 11:05:04 +0900

    feature: 추천 티 리스트 페이지 2차 마크업 - 탭버튼 추가

--- src/pages/Join/MyTastePage.tsx
+++ src/pages/Join/MyTastePage.tsx
@@ -1,21 +1,35 @@
-import { TeaRecommendCard } from "@/components/TeaCard/CardComponents";
+import { TabButton } from '@/components/Buttons/TabButton';
+import { TeaRecommendCard } from '@/components/TeaCard/CardComponents';
+import { useState } from 'react';
 
 const teaData = [
-   { imageUrl: "/image1.jpg", teaName: "얼그레이", brand: "브랜드1" },
-   { imageUrl: "/image2.jpg", teaName: "녹차", brand: "브랜드2" },
-   { imageUrl: "/image3.jpg", teaName: "홍차", brand: "브랜드3" },
-   { imageUrl: "/image4.jpg", teaName: "우롱차", brand: "브랜드4" },
-   { imageUrl: "/image5.jpg", teaName: "페퍼민트", brand: "브랜드5" },
-   { imageUrl: "/image6.jpg", teaName: "카모마일", brand: "브랜드6" },
-   { imageUrl: "/image7.jpg", teaName: "라벤더", brand: "브랜드7" },
-   { imageUrl: "/image8.jpg", teaName: "루이보스", brand: "브랜드8" },
+   { imageUrl: '/image1.jpg', teaName: '얼그레이', brand: '브랜드1' },
+   { imageUrl: '/image2.jpg', teaName: '녹차', brand: '브랜드2' },
+   { imageUrl: '/image3.jpg', teaName: '홍차', brand: '브랜드3' },
+   { imageUrl: '/image4.jpg', teaName: '우롱차', brand: '브랜드4' },
+   { imageUrl: '/image5.jpg', teaName: '페퍼민트', brand: '브랜드5' },
+   { imageUrl: '/image6.jpg', teaName: '카모마일', brand: '브랜드6' },
+   { imageUrl: '/image7.jpg', teaName: '라벤더', brand: '브랜드7' },
+   { imageUrl: '/image8.jpg', teaName: '루이보스', brand: '브랜드8' },
 ];
 
 export default function MyTastePage() {
+   const [selectedTab, setSelectedTab] = useState('홍차');
+   const handleTabSelect = (tab: string) => {
+      setSelectedTab(tab);
+   };
+
+   // 선택된 탭에 따라 차 데이터를 필터링
+   // const filteredTeaData = teaData.filter((tea) => tea.type === selectedTab);
+
    return (
-      <div className="mx-5">
-         {/* TabButton컴포넌트 */}
-         <div className="grid gap-4 grid-cols-[repeat(auto-fill,_minmax(160px,_1fr))]">
+      <div className="m-5 flex flex-col gap-5">
+         <h1 className="sr-only">추천 티 리스트 페이지</h1>
+         <TabButton
+            tabs={['홍차', '우롱차', '녹차', '허브차']}
+            onTabSelect={handleTabSelect}
+         />
+         <div className="grid grid-cols-[repeat(auto-fill,_minmax(160px,_1fr))] gap-4">
             {teaData.map((tea, index) => (
                <div className="flex items-center justify-center">
                   <TeaRecommendCard
