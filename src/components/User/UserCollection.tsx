e939f5b595e5025ca37bf757573906d9be646057
Suubiin <suubiin0819@gmail.com>
2024-09-10 14:18:17 +0900

    UserCollection 컴포넌트 중간 커밋

--- src/components/User/UserCollection.tsx
+++ src/components/User/UserCollection.tsx
@@ -1,20 +1,20 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-interface UserCollectionProps {
-   href: string;
+interface UserCollectionPops{
+
 }
 
-export default function UserCollection({ href }: UserCollectionProps) {
-   return (
-      <Link
-         to={href}
-         className="flex w-full flex-col items-center gap-2 rounded-2xl bg-white p-6"
-      >
-         <span
-            className="fi fi-rr-heart inline-flex text-red-600"
-            aria-hidden={true}
-         ></span>
-         <p className="text-base font-extrabold text-stone-950">나의 찜</p>
+export default function UserCollection() {
+  return (
+    <>
+      <Link to={'/my-page/favorites'} className="bg-white rounded-2xl w-full p-6 flex flex-col gap-2 items-center">
+        <span className='fi fi-rr-heart text-red-600 inline-flex' aria-hidden={true}></span>
+        <p className="text-stone-950 font-extrabold text-base">나의 찜</p>
       </Link>
-   );
+      <Link to={'/my-page/reviews'} className="bg-white rounded-2xl w-full p-6 flex flex-col gap-2 items-center">
+        <span className='fi fi-rr-comment-alt text-green-600 inline-flex' aria-hidden={true}></span>
+        <p className="text-stone-950 font-extrabold text-base">나의 리뷰</p>
+      </Link>
+    </>
+  )
 }
