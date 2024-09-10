b63e48b9c3ac60044254e279bd388b45dde0c065
happyhye <50566@naver.com>
2024-09-10 12:53:29 +0900

    refactor: input 컴포넌트 리팩터링
    - input 컴포넌트 코드리뷰 받은 사항 수정
    - input 컴포넌트 명도 대비 이슈 수정
    - input 컴포넌트 입력값 공백 제거
    - 타입 명시
    - 컴포넌트 사용법 추가
    - 정규식 함수 수정
    - tailwind.config 파일에 boxShadow 스타일 추가
    
    resolves #135

--- src/components/Input/SearchInput.tsx
+++ src/components/Input/SearchInput.tsx
@@ -1,41 +1,56 @@
 /* 검색 input */
 
-import { MouseEventHandler, useId, useState } from 'react';
+/* 사용법
 
-interface Props {
-   onClick: MouseEventHandler<HTMLButtonElement>;
+1. 메인 페이지에서 사용 시 (검색 페이지로 이동하는 역할만 함)
+<SearchInput isButton={true} />
+
+2. 검색 페이지에서 사용 시
+<SearchInput onClick={() => console.log('click')} />
+
+*/
+
+import { MouseEventHandler, useId, useState } from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
+
+export interface Props {
+   isButton?: boolean;
+   onClick?: MouseEventHandler<HTMLButtonElement>;
    [property: string]: any;
 }
 
-function SearchInput({ onClick, ...restProps }: Props) {
+export default function SearchInput({
+   isButton = false,
+   onClick,
+   ...restProps
+}: Props) {
    const [isFocusSearch, setIsFocusSearch] = useState<boolean>(false);
 
    const searchId: string = useId();
 
    const getSearchInputTitle: string = '원하는 티를 검색해보세요!';
-   // 테일윈드 컨피그~
-   const boxShadow: string = `shadow-[0px_2px_20px_0px_rgba(214,211,209,1), 0px_0px_30px_0px_rgba(255,255,255,1)]`;
 
    const borderColor: string = isFocusSearch
-      ? `border-b-green-700`
-      : `border-b-stone-200`;
+      ? 'border-b-green-700'
+      : 'border-b-stone-950';
 
    const iconColor: string = isFocusSearch
-      ? `text-green-700`
-      : `text-stone-400`;
+      ? 'text-green-700'
+      : 'text-stone-950';
 
-   return (
+   // 공통 내용
+   const renderInput = (isButton: boolean): JSX.Element => (
       <div
-         className={`flex h-[3.75rem] rounded-2xl border border-solid border-white bg-white px-6 py-3 ${boxShadow}`}
+         className={`flex h-[3.75rem] rounded-2xl border border-solid border-white bg-white px-6 py-3 shadow-search`}
       >
          <div
-            className={`search-input-group flex flex-grow border-b border-solid ${borderColor}`}
+            className={`search-input-group flex flex-grow border-b border-solid ${isButton ? 'text-stone-950' : borderColor}`}
          >
             <label className="sr-only" htmlFor={searchId}>
-               검색어 입력
+               {isButton ? '검색 페이지로 이동' : '검색어 입력'}
             </label>
             <input
-               className="w-full py-3 text-base font-normal placeholder-stone-400 focus:placeholder-green-700 focus:outline-none"
+               className={`w-full py-3 text-base font-normal placeholder-stone-950 focus:outline-none ${isButton ? 'cursor-pointer' : 'focus:placeholder-green-700'}`.trim()}
                type="search"
                id={searchId}
                name="filter"
@@ -44,21 +59,33 @@ function SearchInput({ onClick, ...restProps }: Props) {
                onFocus={() => setIsFocusSearch(!isFocusSearch)}
                onBlur={() => setIsFocusSearch(!isFocusSearch)}
                {...restProps}
+               readOnly={isButton} // ! 메인에선 읽기 전용으로 보이도록 함
             />
             <button
                className="px-2"
                aria-label="검색"
-               type="submit"
+               type={isButton ? 'button' : 'submit'}
                onClick={onClick}
             >
                <span
-                  className={`fi fi-rr-search flex justify-center ${iconColor}`}
+                  className={`fi fi-rr-search flex justify-center ${isButton ? 'text-stone-950' : iconColor}`}
                   aria-hidden={true}
                />
             </button>
          </div>
       </div>
    );
-}
 
-export default SearchInput;
+   // 메인에서는 search 페이지로 이동하는 역할, 검색 페이지에서는 검색 역할
+   return (
+      <>
+         {isButton ? (
+            <BrowserRouter>
+               <Link to="search">{renderInput(isButton)}</Link>
+            </BrowserRouter>
+         ) : (
+            <form>{renderInput(isButton)}</form>
+         )}
+      </>
+   );
+}
