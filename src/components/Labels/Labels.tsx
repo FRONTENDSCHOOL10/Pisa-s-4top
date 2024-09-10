502e818abb2b78e8ec5dd369c7c7f73beaf2a31c
woolen-h <woolen_h@kakao.com>
2024-09-10 11:21:49 +0900

    refactor: 라벨그룹 라벨로 이동

--- src/components/Labels/Labels.tsx
+++ src/components/Labels/Labels.tsx
@@ -75,3 +75,18 @@ export const LabelButton = memo(
       );
    }
 );
+
+interface LabelGroupProps {
+   labels: { label: string }[];
+   size?: 'small' | 'large';
+}
+
+export function LabelGroup({ labels = [], size = 'large' }: LabelGroupProps) {
+   return (
+      <div className="flex flex-wrap gap-1">
+         {labels.map((labelProps, index) => (
+            <Label key={index} {...labelProps} size={size} />
+         ))}
+      </div>
+   );
+}
