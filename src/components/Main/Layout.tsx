c25cd4f4f0fa1ca2c13e00ac3e4fcecf88f4b2bd
minnje <167839981+minnje@users.noreply.github.com>
2024-09-10 13:41:22 +0900

    refactor: 상수관리

--- src/components/Main/Layout.tsx
+++ src/components/Main/Layout.tsx
@@ -1,28 +1,24 @@
+/* 상단 바 다른 거 쓰는 법 (기본값 hasBackBtn)
+각 페이지 파일에서
+<Layout hasBackBtn/> => 뒤로가기 버튼만 있는 상단 바
+<Layout hasLogo /> => 로고만 있는 상단 바
+<Layout title="얼그레이"/> => 뒤로가기 버튼과 얼그레이가 적힌 상단 바 (title에 변수 string 가능)
+*/
+
 import { Outlet, useLocation } from 'react-router-dom';
 import AppBar, { AppBarProps } from './AppBar';
 import BottomNav from './BottomNav';
 
 interface LayoutProps extends AppBarProps {}
 
-/* 상단 바 다른 거 쓰는 법 (기본값 hasBackBtn)
-   각 페이지 파일에서
-   <Layout hasBackBtn/> => 뒤로가기 버튼만 있는 상단 바
-   <Layout hasLogo /> => 로고만 있는 상단 바
-   <Layout title="얼그레이"/> => 뒤로가기 버튼과 얼그레이가 적힌 상단 바 (title에 변수 string 가능)
-*/
+const HIDE_APP_BAR_PATHS = ['/login', '/my-selection', '/my-taste'];
+const HIDE_BOTTOM_NAV_PATHS = ['/login', '/join', '/my-selection', '/my-taste'];
 
 export default function Layout({ hasBackBtn, hasLogo, title }: LayoutProps) {
    const location = useLocation();
    const { pathname } = location;
-   const hideAppBar = ['/login', '/my-selection', '/my-taste'].includes(
-      pathname
-   );
-   const hideBottonNav = [
-      '/login',
-      '/join',
-      '/my-selection',
-      '/my-taste',
-   ].includes(pathname);
+   const hideAppBar = HIDE_APP_BAR_PATHS.includes(pathname);
+   const hideBottonNav = HIDE_BOTTOM_NAV_PATHS.includes(pathname);
 
    return (
       <>
