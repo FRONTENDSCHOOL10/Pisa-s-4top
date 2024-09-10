83ce110f98044aec11d1a8d0f17e677c525bef5a
Suubiin <suubiin0819@gmail.com>
2024-09-10 17:56:22 +0900

    modify: 타입 명시 수정

--- src/pages/Recommend/TeaListPage.tsx
+++ src/pages/Recommend/TeaListPage.tsx
@@ -2,7 +2,13 @@ import { TabButton } from '@/components/Buttons/TabButton';
 import { TeaRecommendCard } from '@/components/TeaCard/CardComponents';
 // import { useState } from 'react';
 
-const teaData = [
+interface Tea {
+   imageUrl: string;
+   teaName: string;
+   brand: string;
+}
+
+const teaData: Tea[] = [
    { imageUrl: '', teaName: '얼그레이', brand: '브랜드1' },
    { imageUrl: '', teaName: '녹차', brand: '브랜드2' },
    { imageUrl: '', teaName: '홍차', brand: '브랜드3' },
@@ -14,23 +20,25 @@ const teaData = [
 ];
 
 export default function MyTastePage() {
-   // const [selectedTab, setSelectedTab] = useState('홍차');
+   // const [selectedTab, setSelectedTab] = useState<string>('홍차');
    // const handleTabSelect = (tab: string) => {
    //    setSelectedTab(tab);
    // };
 
    // 선택된 탭에 따라 차 데이터를 필터링
-   // const filteredTeaData = teaData.filter((tea) => tea.type === selectedTab);
+   // const filteredTeaData = teaData.filter((tea: Tea) => tea.teaName === selectedTab);
 
    return (
       <div className="mx-5 mb-10 flex flex-col gap-5">
          <h1 className="sr-only">추천 티 리스트 페이지</h1>
          <TabButton
             tabs={['홍차', '우롱차', '녹차', '허브차']}
-            onTabSelect={()=>{console.log('탭 전환')}}
+            onTabSelect={() => {
+               console.log('탭 전환');
+            }}
          />
          <div className="grid grid-cols-[repeat(auto-fill,_minmax(160px,_1fr))] gap-4">
-            {teaData.map((tea, index) => (
+            {teaData.map((tea: Tea, index: number) => (
                <div key={index} className="flex items-center justify-center">
                   <TeaRecommendCard
                      key={index}
