1d7a14fda8b97b39344869aa2515bfbaf6cef215
woolen-h <woolen_h@kakao.com>
2024-09-12 15:48:13 +0900

    style: my-selection 스타일링

--- src/pages/Join/MySelectionPage.tsx
+++ src/pages/Join/MySelectionPage.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { LabelGroup } from '@/components/Labels/Labels';
 import { fetchTasteNoteData } from '@/utils/fetchData';
+import { Button } from '@/components/Buttons/Buttons';
 
 export function Component() {
    const [tasteNoteData, setTasteNoteData] = useState<string[]>([]);
@@ -19,16 +20,18 @@ export function Component() {
    }, []);
 
    return (
-      <main>
-         <h1>
-            <strong>선호하는 맛</strong>을<br />
+      <main className="flex h-screen w-full flex-col items-center justify-center">
+         <h1 className="mt-7 text-2xl">
+            <strong className="font-extrabold">선호하는 맛</strong>을<br />
             선택해주세요.
          </h1>
-         <p>취향에 맞는 차를 추천해드려요!</p>
+         <p className="mt-2">취향에 맞는 차를 추천해드려요!</p>
          <LabelGroup
             labels={tasteNoteData}
-            className="flex w-full justify-center"
+            types="button"
+            className="mt-6 flex w-full justify-center"
          />
+         <Button content="모두 선택했어요!" size="fullWidth" className="mt-6" />
       </main>
    );
 }
