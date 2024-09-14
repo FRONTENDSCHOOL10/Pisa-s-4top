b2484454316a3ab18d8cf34bc4ff4c334b1f8024
happyhye <50566@naver.com>
2024-09-14 20:24:15 +0900

    style: 당신의 취향 스플래쉬 페이지 스타일링 수정
    resolves #261

--- src/pages/Join/MyTastePage.tsx
+++ src/pages/Join/MyTastePage.tsx
@@ -3,65 +3,65 @@ import SplashSwiper from '@/components/TeaCard/SplashSwiper';
 import createUrl from '@/utils/createUrl';
 
 export function Component() {
+   // ! 추후 '상큼한' 당신에게 → 수정하기
    return (
-      <>
-         <main className="flex flex-col justify-center">
-            <h1 className="mb-2 mt-20 text-center text-2xl font-extrabold">
-               상큼한 당신에게, <br />
-               추천하는 차
-            </h1>
-            <span className="text-center text-sm text-stone-400">
-               취향 데이터를 기반으로 추천해드렸어요!
-            </span>
-            <Button
-               content="홈으로 이동하기"
-               size="small"
-               isLink={true}
-               style={{ margin: '2rem' }}
-               href="/"
-               ariaLabel="홈페이지"
-            />
-            <SplashSwiper
-               images={[
-                  createUrl('totd-logo'),
-                  createUrl('typeLogo-sm'),
-                  createUrl('typeLogo-lg'),
-                  createUrl('totd-logo'),
-                  createUrl('typeLogo-sm'),
-                  createUrl('typeLogo-lg'),
-                  createUrl('totd-logo'),
-                  createUrl('typeLogo-sm'),
-                  createUrl('typeLogo-lg'),
-               ]}
-            />
-            <SplashSwiper
-               reverse={true}
-               images={[
-                  createUrl('totd-logo'),
-                  createUrl('typeLogo-sm'),
-                  createUrl('typeLogo-lg'),
-                  createUrl('totd-logo'),
-                  createUrl('typeLogo-sm'),
-                  createUrl('typeLogo-lg'),
-                  createUrl('totd-logo'),
-                  createUrl('typeLogo-sm'),
-                  createUrl('typeLogo-lg'),
-               ]}
-            />
-            <SplashSwiper
-               images={[
-                  createUrl('totd-logo'),
-                  createUrl('typeLogo-sm'),
-                  createUrl('typeLogo-lg'),
-                  createUrl('totd-logo'),
-                  createUrl('typeLogo-sm'),
-                  createUrl('typeLogo-lg'),
-                  createUrl('totd-logo'),
-                  createUrl('typeLogo-sm'),
-                  createUrl('typeLogo-lg'),
-               ]}
-            />
-         </main>
-      </>
+      <main className="center-layout gap-2">
+         <h1 className="text-center text-2xl font-extrabold">
+            상큼한 당신에게,
+            <br />
+            추천하는 차
+         </h1>
+         <p className="text-center text-sm text-stone-400">
+            취향 데이터를 기반으로 추천해드렸어요!
+         </p>
+         <Button
+            className="my-7 w-[18rem]"
+            content="홈으로 이동하기"
+            size="large"
+            isLink={true}
+            href="/"
+            ariaLabel="홈페이지"
+         />
+         <SplashSwiper
+            images={[
+               createUrl('totd-logo'),
+               createUrl('typeLogo-sm'),
+               createUrl('typeLogo-lg'),
+               createUrl('totd-logo'),
+               createUrl('typeLogo-sm'),
+               createUrl('typeLogo-lg'),
+               createUrl('totd-logo'),
+               createUrl('typeLogo-sm'),
+               createUrl('typeLogo-lg'),
+            ]}
+         />
+         <SplashSwiper
+            reverse={true}
+            images={[
+               createUrl('totd-logo'),
+               createUrl('typeLogo-sm'),
+               createUrl('typeLogo-lg'),
+               createUrl('totd-logo'),
+               createUrl('typeLogo-sm'),
+               createUrl('typeLogo-lg'),
+               createUrl('totd-logo'),
+               createUrl('typeLogo-sm'),
+               createUrl('typeLogo-lg'),
+            ]}
+         />
+         <SplashSwiper
+            images={[
+               createUrl('totd-logo'),
+               createUrl('typeLogo-sm'),
+               createUrl('typeLogo-lg'),
+               createUrl('totd-logo'),
+               createUrl('typeLogo-sm'),
+               createUrl('typeLogo-lg'),
+               createUrl('totd-logo'),
+               createUrl('typeLogo-sm'),
+               createUrl('typeLogo-lg'),
+            ]}
+         />
+      </main>
    );
 }
