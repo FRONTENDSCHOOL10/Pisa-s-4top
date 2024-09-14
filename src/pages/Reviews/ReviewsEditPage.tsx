f5dbd115781fc6c04a5110341d76f0f5cf9366c5
woolen-h <woolen_h@kakao.com>
2024-09-14 13:29:45 +0900

    refactor: 리뷰 수정 페이지 리팩토링

--- src/pages/Reviews/ReviewsEditPage.tsx
+++ src/pages/Reviews/ReviewsEditPage.tsx
@@ -32,10 +32,10 @@ export function Component() {
 
    return (
       <main className="flex flex-col items-center px-6">
-         <div className="h-80 w-80 rounded-2xl bg-stone-300">
+         <div className="h-80 w-80 rounded-3xl bg-stone-300">
             <img className="object-cover" src="" alt="" />
          </div>
-         <p className="my-4">nickname</p>
+         <p className="my-4 text-stone-600">nickname</p>
          <StarRatingAverage score={3} />
 
          <TeaTasteCard
@@ -49,10 +49,10 @@ export function Component() {
          <TeaColorCard className="mb-2" />
          <TeaReviewDetailCard title="리뷰 제목" contents="리뷰 내용" />
 
-         <Button content="수정 끝났어요!" size="fullWidth" className="mt-16" />
+         <Button content="리뷰 수정하기" size="fullWidth" className="mt-16" />
 
          <Button
-            content="삭제할래요!"
+            content="삭제"
             size="fullWidth"
             className="mt-2"
             isError={true}
