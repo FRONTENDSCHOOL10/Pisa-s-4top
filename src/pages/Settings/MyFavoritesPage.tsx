8868b5b45ae2b5d3bbf69b3cf3b1177bdef852d6
woolen-h <woolen_h@kakao.com>
2024-09-16 19:01:52 +0900

    feat: 나의 찜 페이지 데이터 연결

--- src/pages/Settings/MyFavoritesPage.tsx
+++ src/pages/Settings/MyFavoritesPage.tsx
@@ -1,16 +1,25 @@
 import { TabButton } from '@/components/Buttons/TabButton';
 import { useEffect, useState } from 'react';
-import { fetchTeaCategoryData } from '@/utils/fetchData';
+import { fetchTeaCategoryData, fetchTeaData } from '@/utils/fetchData';
 import { TeaRecommendCard } from '@/components/TeaCard/CardComponents';
 
 interface TeaCategory {
    id: string;
    category: string;
 }
+
+interface Tea {
+   id: string;
+   tea_name: string;
+   tea_brand: string;
+   tea_image: string;
+   tea_category: string;
+}
+
 export function Component() {
    const [categories, setCategories] = useState<TeaCategory[]>([]);
-   const [selectedCategory, setSelectedCategory] = useState<string>('');
-   // const [reviewData, setReviewData] = useState([]);
+   const [activeTab, setActiveTab] = useState<string>('');
+   const [teaData, setTeaData] = useState<Tea[]>([]);
 
    useEffect(() => {
       const getCategories = async () => {
@@ -19,7 +28,7 @@ export function Component() {
             setCategories(categoryData);
 
             if (categoryData.length > 0) {
-               setSelectedCategory(categoryData[0].category);
+               setActiveTab(categoryData[0].category);
             }
          } catch (error) {
             console.error('Failed to fetch tea category data:', error);
@@ -28,6 +37,25 @@ export function Component() {
 
       getCategories();
    }, []);
+
+   useEffect(() => {
+      const getTeaData = async () => {
+         try {
+            const allTeaData = await fetchTeaData();
+            const filteredTeaData = allTeaData.filter(
+               (tea) => tea.tea_category === activeTab
+            );
+            setTeaData(filteredTeaData);
+         } catch (error) {
+            console.error('Failed to fetch tea data:', error);
+         }
+      };
+
+      if (activeTab) {
+         getTeaData();
+      }
+   }, [activeTab]);
+
    return (
       <main>
          <h1 className="sr-only">나의 찜 페이지</h1>
@@ -36,46 +64,22 @@ export function Component() {
                <TabButton
                   tabs={categories.map((category) => category.category)}
                   onTabSelect={(categoryName) => {
-                     setSelectedCategory(categoryName);
+                     setActiveTab(categoryName);
                   }}
                   className="mb-8 self-start"
+                  activeTab={activeTab}
                />
-               <ul className="grid gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                  <li>
-                     <TeaRecommendCard
-                        imageUrl=""
-                        teaName="티 이름"
-                        brand="티 브랜드"
-                     />
-                  </li>
-                  <li>
-                     <TeaRecommendCard
-                        imageUrl=""
-                        teaName="티 이름"
-                        brand="티 브랜드"
-                     />
-                  </li>
-                  <li>
-                     <TeaRecommendCard
-                        imageUrl=""
-                        teaName="티 이름"
-                        brand="티 브랜드"
-                     />
-                  </li>
-                  <li>
-                     <TeaRecommendCard
-                        imageUrl=""
-                        teaName="티 이름"
-                        brand="티 브랜드"
-                     />
-                  </li>
-                  <li>
-                     <TeaRecommendCard
-                        imageUrl=""
-                        teaName="티 이름"
-                        brand="티 브랜드"
-                     />
-                  </li>
+               <ul className="grid gap-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
+                  {teaData.map((tea) => (
+                     <li key={tea.id}>
+                        <TeaRecommendCard
+                           id={tea.id}
+                           imageUrl={tea.tea_image}
+                           teaName={tea.tea_name}
+                           brand={tea.tea_brand}
+                        />
+                     </li>
+                  ))}
                </ul>
             </div>
          </article>
