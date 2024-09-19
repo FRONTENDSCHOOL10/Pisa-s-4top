9266e587b8b36b4157f8f5cee49cfc747ff7e66b
woolen-h <woolen_h@kakao.com>
2024-09-19 23:16:42 +0900

    feat: my-taste 기능 추가

--- src/pages/Join/MyTastePage.tsx
+++ src/pages/Join/MyTastePage.tsx
@@ -1,15 +1,45 @@
+import { useState, useEffect } from 'react';
 import { Button } from '@/components/Buttons/Buttons';
 import SplashSwiper from '@/components/TeaCard/SplashSwiper';
-import createUrl from '@/utils/createUrl';
+import { fetchTeasByUserSelection, fetchUserTaste } from '@/utils/fetchData'; // fetchUserTaste 함수 import
 
 export function Component() {
-   // ! 추후 '상큼한' 당신에게 → 수정하기
+   const [filteredTeas, setFilteredTeas] = useState([]);
+   const [userTaste, setUserTaste] = useState<string | null>(null); // userTaste 상태 추가
+
+   useEffect(() => {
+      // 로컬 스토리지에서 사용자 정보 가져오기
+      const storedUser = localStorage.getItem('@auth/user');
+      if (storedUser) {
+         const user = JSON.parse(storedUser);
+         const userNickname = user.nickname;
+
+         // 사용자 선택에 기반한 차 데이터 가져오기
+         const getUserTeas = async () => {
+            try {
+               // user_taste 가져오기
+               const taste = await fetchUserTaste(userNickname);
+               setUserTaste(taste); // 가져온 taste를 상태에 저장
+               console.log('User Taste:', taste);
+
+               // 사용자 선택에 기반한 차 목록 가져오기
+               const teas = await fetchTeasByUserSelection(userNickname);
+               console.log('Fetched Teas:', teas);
+               setFilteredTeas(teas);
+            } catch (error) {
+               console.error('Failed to fetch user teas:', error);
+            }
+         };
+
+         getUserTeas();
+      }
+   }, []);
+
    return (
       <main className="center-layout gap-2">
          <h1 className="text-center text-2xl font-extrabold">
-            상큼한 당신에게,
-            <br />
-            추천하는 차
+            {userTaste ? `${userTaste} 당신에게` : '당신에게'}
+            <br /> 추천하는 차
          </h1>
          <p className="text-center text-sm text-stone-400">
             취향 데이터를 기반으로 추천해드렸어요!
@@ -22,46 +52,12 @@ export function Component() {
             href="/main"
             ariaLabel="홈페이지"
          />
-         <SplashSwiper
-            images={[
-               createUrl('totd-logo'),
-               createUrl('typeLogo-sm'),
-               createUrl('typeLogo-lg'),
-               createUrl('totd-logo'),
-               createUrl('typeLogo-sm'),
-               createUrl('typeLogo-lg'),
-               createUrl('totd-logo'),
-               createUrl('typeLogo-sm'),
-               createUrl('typeLogo-lg'),
-            ]}
-         />
-         <SplashSwiper
-            reverse={true}
-            images={[
-               createUrl('totd-logo'),
-               createUrl('typeLogo-sm'),
-               createUrl('typeLogo-lg'),
-               createUrl('totd-logo'),
-               createUrl('typeLogo-sm'),
-               createUrl('typeLogo-lg'),
-               createUrl('totd-logo'),
-               createUrl('typeLogo-sm'),
-               createUrl('typeLogo-lg'),
-            ]}
-         />
-         <SplashSwiper
-            images={[
-               createUrl('totd-logo'),
-               createUrl('typeLogo-sm'),
-               createUrl('typeLogo-lg'),
-               createUrl('totd-logo'),
-               createUrl('typeLogo-sm'),
-               createUrl('typeLogo-lg'),
-               createUrl('totd-logo'),
-               createUrl('typeLogo-sm'),
-               createUrl('typeLogo-lg'),
-            ]}
-         />
+         <div>
+            <SplashSwiper
+               images={filteredTeas.map((tea) => tea.tea_image)}
+               className="!absolute left-0"
+            />
+         </div>
       </main>
    );
 }
