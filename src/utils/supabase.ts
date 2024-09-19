1c5b709bc38548a90c2edcb4135b34642baccc0e
woolen-h <woolen_h@kakao.com>
2024-09-19 15:38:13 +0900

    refactor: 수파베이스 API 로 변경

---
+++ src/utils/supabase.ts
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+export default supabase;
---
+++ src/utils/updateData.ts
@@ -0,0 +1,17 @@
+import supabase from './supabase';
+
+export async function updateReviewData(id: string, updatedReview: any) {
+   const { data, error } = await supabase
+      .from('review')
+      .update(updatedReview)
+      .eq('id', id)
+      .select('*');
+
+   if (error) {
+      console.error('Error updating review data:', error.message);
+      return null;
+   }
+
+   // console.log('Update successful:', data);
+   return data;
+}
