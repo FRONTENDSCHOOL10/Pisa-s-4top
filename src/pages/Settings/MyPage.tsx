afe80c20818a8979c6e71d0c6f4bf3caba36db2b
suubiin <127467411+Suubiin@users.noreply.github.com>
2024-09-24 03:32:55 +0900

    refactor: mypage-사용하지 않는 useRef 삭제

--- src/pages/Settings/MyPage.tsx
+++ src/pages/Settings/MyPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 
